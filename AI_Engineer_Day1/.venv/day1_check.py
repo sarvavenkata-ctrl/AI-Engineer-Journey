ab057c1a7a88fe5e0d7d0cf1f02b31bd8740232f
Sarva Venkata <sarva.venkata@gmail.com>
2026-02-24 08:34:33 +0000

    Remove venv from repo and add .gitignore

--- AI_Engineer_Day1/.venv/day1_check.py
+++
@@ -1,8 +0,0 @@
-import sys
-import numpy as np
-import pandas as pd
-
-print("✅ Python executable:", sys.executable)
-print("✅ NumPy version:", np.__version__)
-print("✅ Pandas version:", pd.__version__)
-print("✅ Setup is working!")
